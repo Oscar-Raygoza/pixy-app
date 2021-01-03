d046f9fa836057d01ea1bee26bad5dd4014e2174
Oscar Eduardo Raygoza <oscar.eduardo.raygoza@gmail.com>
2021-01-02 21:05:14 -0600

    add 404 component

--- src/views/404.js
+++ src/views/404.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 import Layout from "../components/Layout";
+import { Link } from "react-router-dom";
 
-const IndexPage = () => (
+/*
+ * Assets
+ */
+import logo from "../assets/images/logo.svg";
+const Page404 = ({ location }) => (
   <Layout fullMenu>
     <section id="wrapper">
       <header>
         <div className="inner">
-          <h2>Page not found</h2>
-          <p>Not a valid URL</p>
+          <img src={logo} className="image center "/>
+          <h2>Page not found 404</h2>
+          <p>Not a valid URL {location.pathname}</p>
+          <Link to="/" className="button primary large">
+            GO TO HOME
+          </Link>
         </div>
       </header>
     </section>
   </Layout>
 );
 
-export default IndexPage;
+export default Page404;
